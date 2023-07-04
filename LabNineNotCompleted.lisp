85acc634db7cc67c12b09485a31d0fd6604302d6
Haris Sujethan <96924701+haris-sujethan@users.noreply.github.com>
2023-07-04 11:32:32 -0400

    Create LabNineNotCompleted.lisp

---
+++ LabNineNotCompleted.lisp
@@ -0,0 +1,8 @@
+(defun has-loop (g s)
+ ;(let ((ids (sort (g-keys (nodes g)) '<)))
+  ;(dolist (id1 ids)
+   ; (let ((node (gethash id1 (nodes g))))
+     ; (dolist (id2 ids)
+       ;         (find id2 (node-edges node) :key 'edge-dst)))))
+     ; 
+  )
